46cfdc8db1283d14b64eb7eff8e530a0f6af4936
Francisco Sanchez <frango9000@gmail.com>
2020-05-28 21:27:30 +0200

    codeCoverage fix intent

--- src/main/java/com/campusdual/lituraliabackspring/repositories/LibrosRepository.java
+++ src/main/java/com/campusdual/lituraliabackspring/repositories/LibrosRepository.java
@@ -1,10 +1,13 @@
 package com.campusdual.lituraliabackspring.repositories;
 
 import com.campusdual.lituraliabackspring.domain.Libro;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LibrosRepository extends JpaRepository<Libro, Long> {
+
+    Optional<Libro> findByTitulo(String titulo);
 
 }
